8479e8698569159d51709687cd233b7f507e2911
VASITHRA D <126411013+VASITHRAD@users.noreply.github.com>
2023-08-17 22:52:45 +0530

    Create Print n times

---
+++ Print n times.java
@@ -0,0 +1,12 @@
+import java.util.List;
+public class Solution {
+    public static int i=0;
+    public static String[]arr;
+    public static void printNtimes(int n){
+        for(int i=0;i<n;i++){
+        // Write your code here.
+        System.out.print("Coding Ninjas ");
+
+    }
+    }
+    }
